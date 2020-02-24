671039a2f999d897e9cca22408f67db18822dd1b
Akinjide Bankole <r@akinjide.me>
2020-02-24 17:21:30 +0100

    feat: add packagist credentials

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,2 @@
-curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username=USERNAME&apiToken=API_TOKEN' -d'{"repository":{"url":"PACKAGIST_PACKAGE_URL"}}'
+echo "using " $USERNAME
+curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username=$USERNAME&apiToken=$API_TOKEN' -d'{"repository":{"url":"$PACKAGIST_PACKAGE_URL"}}'
